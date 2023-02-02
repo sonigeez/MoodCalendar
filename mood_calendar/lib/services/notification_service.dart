8b31a2a22f0c5b9117e1775dea58987c541894d5
Bharat Soni <bharatchills@gmail.com>
2023-02-02 23:55:23 +0530

    added missing filess

---
+++ mood_calendar/lib/services/notification_service.dart
@@ -0,0 +1,107 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class NotificationService {
+  // Singleton pattern
+  static final NotificationService _notificationService =
+      NotificationService._internal();
+  factory NotificationService() {
+    return _notificationService;
+  }
+
+  NotificationService._internal();
+
+  static const channelId = "1";
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static final AndroidNotificationDetails _androidNotificationDetails =
+      AndroidNotificationDetails(
+    channelId,
+    "Mood Calendar",
+    priority: Priority.high,
+    importance: Importance.high,
+  );
+
+  final NotificationDetails notificationDetails = NotificationDetails(
+    android: _androidNotificationDetails,
+  );
+
+  Future<void> init() async {
+    final AndroidInitializationSettings androidInitializationSettings =
+        AndroidInitializationSettings("background");
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: androidInitializationSettings,
+    );
+
+    // *** Initialize timezone here ***
+    tz.initializeTimeZones();
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+
+    await _scheduleDaily11PMNotification();
+  }
+
+  Future<void> showNotification(int id, String title, String body) async {
+    await flutterLocalNotificationsPlugin.show(
+      id,
+      title,
+      body,
+      notificationDetails,
+      payload: "payload",
+    );
+  }
+
+  tz.TZDateTime _nextInstanceOf11PM() {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduledDate =
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, 23);
+    if (scheduledDate.isBefore(now)) {
+      scheduledDate = scheduledDate.add(const Duration(days: 1));
+    }
+    return scheduledDate;
+  }
+
+  Future<void> _scheduleDaily11PMNotification() async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        'Hey Listen!!!👀',
+        'Have you Journeled your mood today?',
+        _nextInstanceOf11PM(),
+        notificationDetails,
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time);
+  }
+
+  Future<void> scheduleNotification(int id, String title, String body,
+      DateTime eventDate, TimeOfDay eventTime, String payload,
+      [DateTimeComponents? dateTimeComponents]) async {
+    final scheduledTime = eventDate.add(Duration(
+      hours: eventTime.hour,
+      minutes: eventTime.minute,
+    ));
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(scheduledTime, tz.local),
+      notificationDetails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+      payload: payload,
+      matchDateTimeComponents: dateTimeComponents,
+    );
+  }
+}
